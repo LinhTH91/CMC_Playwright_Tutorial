f4eccb123b0ed7af3880348e6579466f062fcada
THLinh4 <linhtrh91@gmail.com>
2025-08-16 10:55:09 +0700

    Practice 03

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Practice 03', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+// 1. Confirm that the user can login successfully using valid credentials
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#password').fill('secret_sauce');
+  await page.locator('#login-button').click();
+  await expect(page.locator("xpath=//span[text()='Products']")).toBeVisible();
+  
+// 2. Confirm that the product list contains exactly six items.
+  await expect(page.locator("//div[@class='inventory_item']")).toHaveCount(6);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+// 3. Confirm that each product in the list displays its price
+  let result: boolean = await eachProductHaveEachPrice(page);
+  expect(result).toBe(true);
+
+// 4. Confirm that the product is successfully added to the cart
+  await page.locator("xpath=//div[text()='Sauce Labs Backpack']/../../..//button[@id='add-to-cart-sauce-labs-backpack']").click();
+  await page.locator("#shopping_cart_container").click();
+  await expect(page.locator("xpath=//div[@class='cart_quantity' and text()='1']/..//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']")).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+async function eachProductHaveEachPrice(page: Page): Promise<boolean> {
+  let count = await page.locator("//div[@class='inventory_item']").count();
+  let result: boolean = false;
+  for (let i = 1; i <= count; i++) {
+      result = await page.locator("xpath=(//div[@class='inventory_item'])[" + i + "]//div[@class='inventory_item_price']" ).isVisible()
+      if (!result)
+        break;
+  }
+  return result;
+}
